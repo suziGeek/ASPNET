7618355ad7833c4c038973b388b139a4d13879f6
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-07-28 17:26:45 -0400

    added product classes

---
+++ ASPNET/Models/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET.Models
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
---
+++ ASPNET/Models/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS");
+        }
+    }
+}
